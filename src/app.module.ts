a9c4dc6e720de2840d4f9cc988f951c65718e659
Eunsol Lee <eunsol@medibloc.org>
2020-08-16 23:03:08 +0900

    setting connection between mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+
+const mongodbDBName = 'lifestyle-management';
+const mongodbUserName = 'test';
+const mongodbPassword = 'qosHek-fivcug-hichy2';
+const mongodbBasicUri = 'cluster0.mmfgl.mongodb.net';
+const mongodbOptions = 'retryWrites=true&w=majority';
+const mongodbFullUri = `mongodb+srv://${mongodbUserName}:${mongodbPassword}@${mongodbBasicUri}/${mongodbDBName}?${mongodbOptions}`;
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot(mongodbFullUri)],
   controllers: [AppController],
   providers: [AppService],
 })
