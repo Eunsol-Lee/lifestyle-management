24e8bd530f20477b00a18bed022f1f333a9ef96b
Eunsol <eunsol@medibloc.org>
2020-08-19 22:29:18 +0900

    adding dotenv

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+// It loads .env
+import { config } from 'dotenv';
+config();
+
+// console.log(process.env.USERNAME);
+// console.log(process.env.PWD);
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
